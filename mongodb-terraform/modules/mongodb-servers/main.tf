621ce2fe6ed206c5154c80f1d84a42575197ed64
meerali2 <meeralan6989@gmail.com>
2025-06-27 13:16:15 +0530

    Create main.tf

---
+++ mongodb-terraform/modules/mongodb-servers/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "mongodb" {
+  count                  = 2
+  ami                    = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
+  instance_type          = "t2.micro"
+  subnet_id              = var.private_subnet_ids[count.index]
+  vpc_security_group_ids = [var.mongodb_sg_id]
+  key_name               = var.key_name
+
+  tags = {
+    Name = "mongodb-server-${count.index + 1}"
+  }
+}

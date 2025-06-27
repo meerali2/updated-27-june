84b4de21a75a4639a205ac61d6fa56ed453e4dbc
meerali2 <meeralan6989@gmail.com>
2025-06-27 14:51:25 +0530

    Update main.tf

--- mongodb-terraform/modules/mongodb-servers/main.tf
+++ mongodb-terraform/modules/mongodb-servers/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "mongodb" {
   count                  = 2
-  ami                    = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
+  ami                    = "ami-0ec1bf4a8f92e7bd1"  # ubuntu
   instance_type          = "t2.micro"
   subnet_id              = var.private_subnet_ids[count.index]
   vpc_security_group_ids = [var.mongodb_sg_id]
